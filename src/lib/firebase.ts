2694135c0b945c836e5872cc3eb90594b979d89d
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2025-08-17 23:17:24 +0000

    Can you create a dev environment with a database and tables in firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+const prodConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -10,6 +10,18 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
+const devConfig = {
+    apiKey: process.env.NEXT_PUBLIC_DEV_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_DEV_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_DEV_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_DEV_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_DEV_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_DEV_FIREBASE_APP_ID,
+};
+
+const firebaseConfig = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
 
 // Initialize Firebase
 let app;
